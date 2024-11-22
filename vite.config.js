89b9c9ab54874757af73ab8b6638f31e58455fc9
Adam Stirtan <adamstirtan@gmail.com>
2024-11-21 20:50:03 -0500

    Deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gaa-gymnasium-av/',
   plugins: [
     vue({
       template: { transformAssetUrls }
